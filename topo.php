3eec3a33c0dbb0d61aa091342cdadfe52d8297cf
maxccavalcante <150029435+maxccavalcante@users.noreply.github.com>
2023-11-21 08:02:24 -0300

    Delete topo.php
    excluir

--- topo.php
+++
@@ -1,6 +0,0 @@
-<div style="background-color: white;">
-<img src="https://yt3.googleusercontent.com/ytc/APkrFKZtJSrGBhxpyudcaj_h6KJ88Z-n2DG82U72BeDjnw=s900-c-k-c0x00ffffff-no-rj" alt="Gif" style="width:250px;height:250px;">
-
-
-
-</div>
